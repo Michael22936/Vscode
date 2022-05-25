4ded99b1ff7e20540978ab5f480b1f739f7bf6c0
Michael McDonnell <Michael.McDonnell067@gmail.com>
2022-05-25 10:22:59 -0500

    finished functiiionality of qrcode and completed project

--- react-qr/src/App.jsx
+++ react-qr/src/App.jsx
@@ -1,12 +1,37 @@
+import QRCode from 'qrcode'
+import { useState } from 'react'
 
 function App() {
 
+	const [url, setUrl] = useState('')
+	const [qrcode, setQrcode] = useState('')
+
+	const GenerateQRCode = () => {
+		QRCode.toDataURL(url, {
+			width: 800,
+			margin: 2,
+			color: {
+				dark: '#335383ff'
+			}
+		}, (err, url) => {
+			if (err) return console.error(err)
+			
+			setQrcode(url)
+		})
+	}
+
 	return (
-		<div className="app">
+		<div className = "app">
 			<h1>QR Code Generator</h1>
-			<input type="text" 
-					placeholder="Enter URL"/>
-					<button>Generate</button>
+			<input type = "text" 
+					placeholder = "e.g. https://google.com"
+					value = { url }
+					onChange = { (evt) => setUrl(evt.target.value) }/>
+					<button onClick = { GenerateQRCode } >Generate</button>
+					{qrcode && <>
+						<img src = { qrcode } />
+						<a href = {qrcode} download = "qrcode.png" >Download</a>
+					</>}
 		</div>
 	)
 }
