450a689b98d61ed0ed1c64dec15337cd82442d98
Michael McDonnell <Michael.McDonnell067@gmail.com>
2022-05-25 09:56:26 -0500

    finish initial css

--- react-qr/src/App.jsx
+++ react-qr/src/App.jsx
@@ -3,7 +3,10 @@ function App() {
 
 	return (
 		<div className="app">
-			<h1>Hello World</h1>
+			<h1>QR Code Generator</h1>
+			<input type="text" 
+					placeholder="Enter URL"/>
+					<button>Generate</button>
 		</div>
 	)
 }
